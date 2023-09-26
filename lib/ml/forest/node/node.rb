e7d65ca5511fdede85b27535fcba491196e83ffa
Adam Ulrich <adamo.ulrich@gmail.com>
2023-09-26 15:43:00 +0200

    added minmaxgroups

--- lib/ml/forest/node/node.rb
+++ lib/ml/forest/node/node.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
 module Node
-
-  def self.init_from_data(data, forest_helper:)
-    return OutNode.new(data) if forest_helper.end_condition(data)
+  def self.init_from_data(data, minmaxgroups = data.data.transpose.map(&:minmax).transpose, forest_helper:)
+    return OutNode.new(data, minmaxgroups) if forest_helper.end_condition(data)
 
     split_point = forest_helper.split_point(data)
     node_groups = forest_helper.group(data, split_point)
 
     InNode.new(
       node_groups.transform_values do |group|
-        init_from_data(group, forest_helper: forest_helper)
+        minmaxgroup = group.data.transpose.map(&:minmax).transpose
+        init_from_data(group, minmaxgroup, forest_helper: forest_helper)
       end,
-      split_point
+      split_point,
+      minmaxgroups
     )
   end
 
@@ -24,13 +25,13 @@ module Node
     walk_nodes(node.branches[next_branch_key], element, forest_helper: forest_helper)
   end
 
-  OutNode = Data.define(:data) do
+  OutNode = Data.define(:data, :minmaxborders) do
     def to_a
       data
     end
   end
 
-  InNode = Data.define(:branches, :split_point) do
+  InNode = Data.define(:branches, :split_point, :minmaxborders) do
     def to_a
       branches.values.map(&:to_a)
     end
